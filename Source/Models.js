43d3b050a8f1aef0c5c313065a4be8f9ca2a4211
zeus <zeusofolympus1000@gmail.com>
2022-07-15 09:29:41 -0400

    description of models

---
+++ Source/Models.js
@@ -0,0 +1,8 @@
+//Models take a vector, finds distiction space between charachteristics
+//in the vector, and makes predictions based on the distinction space
+//in high or low dimensionality
+class Model{
+	constructor(){
+
+	}
+}
