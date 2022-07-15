329c038677e5e02631c8f2d70556a67552ebfa84
zeus <zeusofolympus1000@gmail.com>
2022-07-15 09:34:29 -0400

    more info on models

--- Source/Models.js
+++ Source/Models.js
@@ -1,6 +1,10 @@
 //Models take a vector, finds distiction space between charachteristics
 //in the vector, and makes predictions based on the distinction space
-//in high or low dimensionality
+//in high or low dimensionality.
+//Basically, a model will plot the vector space in whatever dimension
+//the model works with, and try to find groupings or interdimensional
+//lines that fits the plotted data, and then make a classification
+//prediction based on the lines drawn between the points.
 class Model{
 	constructor(){
 
