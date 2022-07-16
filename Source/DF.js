03f83dbe966adf296fd5748e09a7017b9537330e
zeus <zeusofolympus1000@gmail.com>
2022-07-16 00:22:25 -0400

    groupOnRow() is virtually the same, but would work differently because we specifiy row wise schema

--- Source/DF.js
+++ Source/DF.js
@@ -54,9 +54,15 @@ class DF{
 		
 	}
 
-	groupOnC(columnName, maxRows, replacementVal){
+	groupOnCol(columnName, maxRows, replacementVal){
 		// groups on any column with columnName, so long as they all share the same value //default if value shared
 		// If they don't have the same value, you can replace the value	//expected if value not shared
-		//maxRows is set to infinity if not specified, and reorders the rows to fit the group
+		//maxRows is set to infinity if not specified, and reorders the rows to fit the col group
+	}
+
+	groupOnRow(rowName, maxCols, replacementVal){
+		// groups on any row with rowName, so long as they all share the same value //default if value shared
+		// If they don't have the same value, you can replace the value	//expected if value not shared
+		//maxCols is set to infinity if not specified, and reorders the cols to fit the row group
 	}
 }
