25f84f852a3be5d8fd908b0d650cc214191f66ed
zeus <zeusofolympus1000@gmail.com>
2022-07-16 07:33:11 -0400

    sort step diagram

--- Source/_DF.js
+++ Source/_DF.js
@@ -1,30 +1,44 @@
 class _DF{
 	constructor(){
-
+		this._df=undefined;
 	}
-	_groupOnRowIndexGreedyRepl(indexName, replVal){}
-	_groupOnRowIndexGreedy(indexName){}
-	_groupOnRowIndexRepl(indexName, maxrc, replVal){}
-	_groupOnRowIndex(indexName, maxrc){}
-	_groupOnAllRowsGreedyRepl(replVal){}
-	_groupOnAllRowsGreedy(){}
-	_groupOnAllRowsRepl(maxrc, replVal){}
-	_groupOnAllRows(maxrc){}
+	_sortOnRowFrom(row, c1, c2){}
+	_sortOnCol(col, r1, r2){}
+	_sortOnRowTime(row, c1, c2){}
+	_sortOnColTime(col, r1, r2){}
 
+	_sortStepOnColFrom(col, r1, r2){
+		//
+		// | sort 
+		// v until
+		//dupes 	--> |sort
+		// 				|until
+		//				v
+		//				sub-dupes
+		// | sort
+		// v until
+		//dupes		==> |
+		//				|
+		//				v
+		//				sub-dupes
+	}
+
+	_applyOnRow()
+
+	_groupOnRowIndexGreedyRepl(indexName, replVal){}
 	_groupOnColIndexGreedyRepl(indexName, replVal){}
+	_groupOnRowIndexGreedy(indexName){}
 	_groupOnColIndexGreedy(indexName){}
+	_groupOnRowIndexRepl(indexName, maxrc, replVal){}
 	_groupOnColIndexRepl(indexName, maxrc, replVal){}
+	_groupOnRowIndex(indexName, maxrc){}
 	_groupOnColIndex(indexName, maxrc){}
 	_groupOnAllColsGreedyRepl(replVal){}
+	_groupOnAllRowsGreedyRepl(replVal){}
 	_groupOnAllColsGreedy(){}
+	_groupOnAllRowsGreedy(){}
 	_groupOnAllColsRepl(maxrc, replVal){}
+	_groupOnAllRowsRepl(maxrc, replVal){}
 	_groupOnAllCols(maxrc){}
-
-
-	_getRowsOn(rowIndex){
-		//
-	}
-	_getColsOn(colIndex){
-
-	}
+	_groupOnAllRows(maxrc){}
 }
